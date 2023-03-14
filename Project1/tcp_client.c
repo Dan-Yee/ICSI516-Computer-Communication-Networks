be93233869f7c6d2fbb8b3a683a60252466733c4
Dan-Yee <dyee3435@gmail.com>
2023-03-13 21:35:04 -0400

    add tcp client and server with required implementations

--- Project1/tcp_client.c
+++ Project1/tcp_client.c
@@ -9,6 +9,11 @@
  * usage: tcp_client <host> <port>
  */
 
+/*
+ * Code edited for Project 1 in ICSI 516
+ * Author: Dan Yee
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,6 +41,12 @@ int main(int argc, char **argv) {
     char *hostname;
     char buf[BUFSIZE];
 	struct timespec start, end;
+
+    FILE* output = fopen("output.txt", "w");            // stores the output returned from the server
+    char* splitStr;
+    char recvCountBuf[BUFSIZE];
+    int recvCount = 0;
+    int writeAmount;
 	
     /* check command line arguments */
     if (argc != 3) {
@@ -60,32 +71,64 @@ int main(int argc, char **argv) {
     /* build the server's Internet address */
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
 
     /* connect: create a connection with the server */
     if (connect(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0) 
-      error("ERROR connecting");
+        error("ERROR connecting");
 
     /* get message line from the user */
-    printf("Please enter msg: ");
+    printf("Enter file name and path: ");
     bzero(buf, BUFSIZE);
     fgets(buf, BUFSIZE, stdin);
     
-    /* send the message line to the server */
+    /* send the file name and path to the server */
     n = write(sockfd, buf, strlen(buf));
     if (n < 0) 
-      error("ERROR writing to socket");
+        error("ERROR writing to socket");
 
     /* print the server's reply */
-    bzero(buf, BUFSIZE);
-    n = read(sockfd, buf, BUFSIZE);
-    if (n < 0) 
-      error("ERROR reading from socket");
-    printf("Echo from server: %s", buf);
-	
-    close(sockfd);
+    // loop until the special char "$" is received
+    while (strcmp(buf, "$") != 0)
+    {
+        bzero(buf, BUFSIZE);
+        n = read(sockfd, buf, BUFSIZE);
+        if (n < 0) 
+            error("ERROR reading from socket");
+
+        // need to split the read buffer by \n because read() reads more than each line sent by the server
+        splitStr = strtok(buf, "\n");
+        while (splitStr != NULL && strcmp(splitStr, "$") != 0)
+        {
+            writeAmount = fwrite(splitStr, sizeof(char), strlen(splitStr), output);     // write the read line to file
+            if (writeAmount < 0)
+                error("ERROR writing to file");
+            writeAmount = fwrite("\n\0", sizeof(char), strlen("\n\0"), output);         // write the \n char, which was removed by strtok()
+            if (writeAmount < 0)
+                error("ERROR writing to file");
+            splitStr = strtok(NULL, "\n");                                              // get the next instance of \n
+            recvCount++;
+
+            if (recvCount % 4 == 0)                                                     // for every 4 messages received, send an "ACK"-like message to the server
+            {
+                sprintf(recvCountBuf, "%d", recvCount);
+                strcpy(buf, "Received messages up to # \0");
+                strcat(buf, recvCountBuf);
+                strcat(buf, "\0");
+
+                n = write(sockfd, buf, strlen(buf));
+                if (n < 0)
+                    error("ERROR writing to socket");
+            }
+        }
+    }
+    fclose(output);                                         // close the output file
+    close(sockfd);                                          // close the socket
+    
+    if (recvCount == 0)
+        printf("Error: Requested file not found on Server.\n");
+    else
+        printf("File data transfer complete.\n");
     return 0;
 }
-
--- Project1/tcp_server.c
+++ Project1/tcp_server.c
@@ -9,6 +9,11 @@
  * usage: tcp_server <port>
  */
 
+/*
+ * Code edited for Project 1 in ICSI 516
+ * Author: Dan Yee
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -21,126 +26,151 @@
 
 #define BUFSIZE 1024
 
-
 /*
  * error - wrapper for perror
  */
 void error(char *msg) {
-  perror(msg);
-  exit(1);
+    perror(msg);
+    exit(1);
 }
 
 int main(int argc, char **argv) {
-  int parentfd; /* parent socket */
-  int childfd; /* child socket */
-  int portno; /* port to listen on */
-  socklen_t clientlen; /* byte size of client's address */
-  struct sockaddr_in serveraddr; /* server's addr */
-  struct sockaddr_in clientaddr; /* client addr */
-  struct hostent *hostp; /* client host info */
-  char buf[BUFSIZE]; /* message buffer */
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
-
-  /* 
-   * check command line arguments 
-   */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-  portno = atoi(argv[1]);
-
-  /* 
-   * socket: create the parent socket 
-   */
-  parentfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (parentfd < 0) 
-    error("ERROR opening socket");
-
-  /* setsockopt: Handy debugging trick that lets 
-   * us rerun the server immediately after we kill it; 
-   * otherwise we have to wait about 20 secs. 
-   * Eliminates "ERROR on binding: Address already in use" error. 
-   */
-  optval = 1;
-  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, 
-	     (const void *)&optval , sizeof(int));
-
-  /*
-   * build the server's Internet address
-   */
-  bzero((char *) &serveraddr, sizeof(serveraddr));
-
-  /* this is an Internet address */
-  serveraddr.sin_family = AF_INET;
-
-  /* let the system figure out our IP address */
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-  /* this is the port we will listen on */
-  serveraddr.sin_port = htons((unsigned short)portno);
-
-  /* 
-   * bind: associate the parent socket with a port 
-   */
-  if (bind(parentfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) 
-    error("ERROR on binding");
-
-  /*
-   * Needed for TCP
-   * listen: make this socket ready to accept connection requests 
-   */
-  if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
-    error("ERROR on listen");
-
-  /* 
-   * main loop: wait for a connection request, echo input line, 
-   * then close connection.
-   */
-  clientlen = sizeof(clientaddr);
-  while (1) {
-
-    /* 
-     * accept: wait for a connection request 
-     */
-    childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
-    if (childfd < 0) 
-      error("ERROR on accept");
+    int parentfd; /* parent socket */
+    int childfd; /* child socket */
+    int portno; /* port to listen on */
+    socklen_t clientlen; /* byte size of client's address */
+    struct sockaddr_in serveraddr; /* server's addr */
+    struct sockaddr_in clientaddr; /* client addr */
+    struct hostent *hostp; /* client host info */
+    char buf[BUFSIZE]; /* message buffer */
+    char *hostaddrp; /* dotted decimal host addr string */
+    int optval; /* flag value for setsockopt */
+    int n; /* message byte size */
     
-    /* 
-     * gethostbyaddr: determine who sent the message 
-     */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL)
-      error("ERROR on gethostbyaddr");
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
-    if (hostaddrp == NULL)
-      error("ERROR on inet_ntoa\n");
-    printf("server established connection with %s (%s)\n", 
-	   hostp->h_name, hostaddrp);
-    
-    /* 
-     * read: read input string from the client
-     */
-    bzero(buf, BUFSIZE);
-    n = read(childfd, buf, BUFSIZE);
-    if (n < 0) 
-      error("ERROR reading from socket");
-    printf("server received %d bytes: %s", n, buf);
-    
-    /* 
-     * write: echo the input string back to the client 
-     */
-    n = write(childfd, buf, strlen(buf));
-    if (n < 0) 
-      error("ERROR writing to socket");
+    FILE* requestedFile;
+    char* specialChar = "$\0";
+    int sendCount = 0;
 
-    close(childfd);
-  }
-  close(parentfd);
+    /* 
+    * check command line arguments 
+    */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    portno = atoi(argv[1]);
+
+    /* 
+    * socket: create the parent socket 
+    */
+    parentfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (parentfd < 0) 
+        error("ERROR opening socket");
+
+    /* setsockopt: Handy debugging trick that lets 
+    * us rerun the server immediately after we kill it; 
+    * otherwise we have to wait about 20 secs. 
+    * Eliminates "ERROR on binding: Address already in use" error. 
+    */
+    optval = 1;
+    setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+
+    /*
+    * build the server's Internet address
+    */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+
+    /* this is an Internet address */
+    serveraddr.sin_family = AF_INET;
+
+    /* let the system figure out our IP address */
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    /* this is the port we will listen on */
+    serveraddr.sin_port = htons((unsigned short)portno);
+
+    /* 
+    * bind: associate the parent socket with a port 
+    */
+    if (bind(parentfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
+        error("ERROR on binding");
+
+    /*
+    * Needed for TCP
+    * listen: make this socket ready to accept connection requests 
+    */
+    if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
+        error("ERROR on listen");
+
+    /* 
+    * main loop: wait for a connection request, echo input line, 
+    * then close connection.
+    */
+    clientlen = sizeof(clientaddr);
+    while (1) {
+        /* 
+         * accept: wait for a connection request 
+         */
+        childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+        if (childfd < 0) 
+            error("ERROR on accept");
+
+        /* 
+         * gethostbyaddr: determine who sent the message 
+         */
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        if (hostp == NULL)
+            error("ERROR on gethostbyaddr");
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        if (hostaddrp == NULL)
+            error("ERROR on inet_ntoa\n");
+        printf("Server Established connection with %s (%s)\n", hostp->h_name, hostaddrp);
+
+        /* 
+         * read: read requested file name and path string from the client
+         */
+        bzero(buf, BUFSIZE);
+        n = read(childfd, buf, BUFSIZE);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        
+        buf[strcspn(buf, "\n")] = 0;                            // strip the \n char off to make sure the file name is valid
+        printf("Client Requested File: %s\n", buf);
+        requestedFile = fopen(buf, "r");                        // attempt to open the requested file
+        if (requestedFile == NULL)
+            printf("Error: No Such File or Directory");
+
+        // read until end of the file if file was successfully opened
+        while (requestedFile != NULL && fgets(buf, BUFSIZE, requestedFile))
+        {
+            printf("\tLine: %s", buf);
+            n = write(childfd, buf, strlen(buf));
+            if (n < 0)
+                error("ERROR writing to socket (file reading)");
+
+            sendCount++;
+            if (sendCount == 4)                             // for every 4 lines sent, wait for an ACK from the Client
+            {
+                bzero(buf, BUFSIZE);
+                n = read(childfd, buf, BUFSIZE);
+                if (n < 0)
+                    error("ERROR reading from socket");
+                printf("\n%s\n", buf);
+                sendCount = 0;
+            }
+        }
+
+        // close the file if it was successfully opened (otherwise, segfault)
+        if (requestedFile)
+            fclose(requestedFile);
+
+        // Send the special char to the client to indicate end of file
+        // Character is sent immediately if requested file cannot be opened
+        n = write(childfd, specialChar, strlen(specialChar));
+        if (n < 0) 
+            error("ERROR writing to socket");
+        close(childfd);
+        printf("\n");
+    }
+    close(parentfd);
 }
-
